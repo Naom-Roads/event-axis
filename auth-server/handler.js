a1745fc38bfd1891237b7be8823d4d7e0757957a
Naom-Roads <nrrodrig@gmail.com>
2021-12-08 16:57:30 -0800

    Added Access Token Function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -39,3 +39,33 @@ module.exports.getAuthURL = async () => {
         }),
     };
 };
+
+module.exports.getAccessToken = async (event) => {
+    const oAuth2Client = new google.auth.OAuth2(
+        client_id,
+        client_secret,
+        redirect_uris[0]
+    );
+    const code = decodeURIComponent( `${event.pathParameters.code}`);
+    return new Promise((resolve, reject) => {
+        oAuth2Client.getToken(code, (err, token) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(token);
+        });
+    })
+        .then((token) => {
+            return {
+                statusCode: 200,
+                body: JSON.stringify(token),
+            };
+        })
+        .catch((err) => {
+            console.error(err);
+            return {
+                statusCode: 500,
+                body: JSON.stringify(err),
+            };
+        });
+};
