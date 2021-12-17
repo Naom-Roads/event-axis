41c3c66e552b177a4e1546fdec7494cb025378f6
Naom-Roads <nrrodrig@gmail.com>
2021-12-17 10:15:42 -0800

    Updated City Search Test and added MockData to the Eventlist Test

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,15 +1,45 @@
 import React, { Component } from "react";
 
+
 class CitySearch extends Component {
+    state = {
+        query: 'Munich',
+        suggestions: []
+    }
+
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        const suggestions = this.props.locations.filter((location) => {
+            return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
+        });
+        this.setState({ query: value, suggestions });
+    };
+
+    handleItemClicked = (suggestion) => {
+        this.setState({
+            query: suggestion
+        });
+    }
+
     render() {
         return (
             <div className="CitySearch">
             <input
                 type="text"
                 className="city"
+                value={this.state.query}
+                onChange={this.handleInputChanged}
                 />
                 <ul className="suggestions">
-
+                    {this.state.suggestions.map((suggestion) => (
+                        <li
+                            key={suggestion}
+                            onClick={() => this.handleItemClicked(suggestion)}>
+                            {suggestion}</li>
+                    ))}
+                    <li key='all'>
+                        <b>See All Cities</b>
+                    </li>
                 </ul>
             </div>
         );
--- src/_tests_/EventList.test.js
+++ src/_tests_/EventList.test.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import EventList from '../EventList';
 import Event from '../Event';
+import { mockData } from "../mock-data";
 
 describe('<EventList /> component', () => {
     test('render correct number of events', () => {
-        const EventListWrapper = shallow(<EventList events={[{id: 1},
-            {id: 2}, { id: 3}, {id: 4}, {id: 5}]} />);
-    expect(EventListWrapper.find(Event)).toHaveLength(5);
+        const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
 });
