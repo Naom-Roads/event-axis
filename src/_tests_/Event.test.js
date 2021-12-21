ca92d6488ec6c07fea78663cbda3659c2c51fc38
Naom-Roads <nrrodrig@gmail.com>
2021-12-21 13:18:16 -0800

    Completed testing for event

--- src/_tests_/Event.test.js
+++ src/_tests_/Event.test.js
@@ -6,7 +6,6 @@ import { oneEvent } from "../mock-data";
 describe('<Event /> component', () => {
     const EventWrapper = shallow(<Event event={oneEvent} />);
     test('display more details when clicked', () => {
-        console.log(EventWrapper.find('button'));
         EventWrapper.find('button').at(0).simulate('click');
         expect(EventWrapper.state('details')).toBeTruthy();
     });
