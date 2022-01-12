9c25338b2c5be4dc1711c3b3240cb584039eded7
Naom-Roads <nrrodrig@gmail.com>
2022-01-12 10:58:34 +0100

    Removed old build and recreated new one, added new files to docs

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/event-axis/precache-manifest.69ebffd43e93bd9fad5093a15093dd8a.js"
+  "/event-axis/precache-manifest.48e073ae105c7ab0ba3fbed4cc297650.js"
 );
 
 workbox.clientsClaim();
