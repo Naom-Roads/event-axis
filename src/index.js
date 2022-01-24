08bec6f47c6464b54d6e14890c558849af9fac8b
Naom-Roads <nrrodrig@gmail.com>
2022-01-24 05:23:41 -0800

    Removed additional line for service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as serviceWorker from './service-worker';
 import reportWebVitals from './reportWebVitals';
 import * as atatus from 'atatus-spa';
@@ -16,7 +15,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
 serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
