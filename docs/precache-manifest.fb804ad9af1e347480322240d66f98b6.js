74292eafa133f4fcf4c105df4e72f32b226c5819
Naom-Roads <nrrodrig@gmail.com>
2022-01-24 05:15:58 -0800

    Fixed Docs file

---
+++ docs/precache-manifest.fb804ad9af1e347480322240d66f98b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "631f501d94d0337cb769",
+    "url": "/event-axis/static/css/main.94f86f9b.chunk.css"
+  },
+  {
+    "revision": "631f501d94d0337cb769",
+    "url": "/event-axis/static/js/main.21c827c6.chunk.js"
+  },
+  {
+    "revision": "4e2d7c9773be81113623",
+    "url": "/event-axis/static/js/runtime~main.9074588e.js"
+  },
+  {
+    "revision": "60e7c1bb8ddcbfe33d4a",
+    "url": "/event-axis/static/js/2.ce52f88f.chunk.js"
+  },
+  {
+    "revision": "49a3e1a6257c00b69b26",
+    "url": "/event-axis/static/js/3.6e3511c1.chunk.js"
+  },
+  {
+    "revision": "c4cd34f37b1a7fbd8f02576d01632242",
+    "url": "/event-axis/index.html"
+  }
+];
