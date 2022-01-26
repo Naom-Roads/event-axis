8f6011d78a2f4155e95f7967ed69df4564531df3
Naom-Roads <nrrodrig@gmail.com>
2022-01-26 17:37:03 +0100

    Removed await in api code to clear lambda function error

---
+++ build/precache-manifest.a8d2173a43c9e9df1b658321057f7703.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2230c16f8ba8c97d375",
+    "url": "/event-axis/static/css/main.d95c9f24.chunk.css"
+  },
+  {
+    "revision": "c2230c16f8ba8c97d375",
+    "url": "/event-axis/static/js/main.1b7ae142.chunk.js"
+  },
+  {
+    "revision": "cc500f12ae5e508feec7",
+    "url": "/event-axis/static/js/runtime~main.17ec6973.js"
+  },
+  {
+    "revision": "53495fbaf564fb8029ec",
+    "url": "/event-axis/static/js/2.3ffb7ce6.chunk.js"
+  },
+  {
+    "revision": "461ae141c5088bafdc9e",
+    "url": "/event-axis/static/js/3.ce65eebf.chunk.js"
+  },
+  {
+    "revision": "ba3b24086dd05717a0c31497a828267c",
+    "url": "/event-axis/index.html"
+  }
+];
