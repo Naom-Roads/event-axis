feea90ea5bae8edfea949305729fdb1cc4115b24
Naom-Roads <nrrodrig@gmail.com>
2022-01-31 14:22:50 +0100

    Added more stying to the welcome page

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -4,11 +4,25 @@ import './WelcomeScreen.scss';
 function WelcomeScreen(props) {
 	return props.showWelcomeScreen ?
 		(
+			<div className="container mb-0">
 			<div className="WelcomeScreen">
-				<h1>Welcome to EventAxis</h1>
-				<h4>
-					Log in to see upcoming events
-				</h4>
+
+				<div className="card-body">
+
+				<h1 className="shadows">
+					<span>e</span>
+					<span>v</span>
+					<span>e</span>
+					<span>n</span>
+					<span>t</span>
+					<span>a</span>
+					<span>x</span>
+					<span>i</span>
+					<span>s</span></h1>
+				</div>
+					<h4>
+						Log in to see upcoming events
+					</h4>
 				<div className="button_cont" align="center">
 					<div className="google-btn">
 						<div className="google-icon-wrapper">
@@ -16,7 +30,7 @@ function WelcomeScreen(props) {
 							     src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Log
 o.svg"
 							     alt="Google sign-in"/>
-						</div>
+
 						<button onClick={() => {
 							props.getAccessToken();
 						}}
@@ -30,6 +44,8 @@ o.svg"
 				<a href="https://Naom-Roads.github.io/event-axis/privacypolicy.html">
 					Privacy Policy
 				</a>
+			</div>
+			</div>
 			</div>
 		)
 		: null;
