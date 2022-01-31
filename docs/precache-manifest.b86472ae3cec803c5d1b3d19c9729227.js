7559d7c7e31a1a0b59cc6287e5b169dc86a5523d
Naom-Roads <nrrodrig@gmail.com>
2022-01-31 14:57:47 +0100

    Added build files to docs

---
+++ docs/precache-manifest.b86472ae3cec803c5d1b3d19c9729227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "02d8f85bea8d2e34dd9e",
+    "url": "/event-axis/static/css/main.b1ffc910.chunk.css"
+  },
+  {
+    "revision": "02d8f85bea8d2e34dd9e",
+    "url": "/event-axis/static/js/main.1b7d3cb3.chunk.js"
+  },
+  {
+    "revision": "cc500f12ae5e508feec7",
+    "url": "/event-axis/static/js/runtime~main.17ec6973.js"
+  },
+  {
+    "revision": "d0b0e272615b767a0c77",
+    "url": "/event-axis/static/js/2.956fa486.chunk.js"
+  },
+  {
+    "revision": "461ae141c5088bafdc9e",
+    "url": "/event-axis/static/js/3.ce65eebf.chunk.js"
+  },
+  {
+    "revision": "4adeed6b86577a81c2f692c19f8722e3",
+    "url": "/event-axis/index.html"
+  }
+];
