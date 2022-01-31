0aaba9e2c13be90bc75b48f730ad2f8a95737710
Naom-Roads <nrrodrig@gmail.com>
2022-01-31 15:54:58 +0100

    Removed 200 px of margin from card-body in event list and rebuilt docs

---
+++ build/precache-manifest.aae0a43f34103042e0d5da5369534a0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73ea4f22efcbccb987d8",
+    "url": "/event-axis/static/css/main.6a19cdd9.chunk.css"
+  },
+  {
+    "revision": "73ea4f22efcbccb987d8",
+    "url": "/event-axis/static/js/main.7d9ba161.chunk.js"
+  },
+  {
+    "revision": "cc500f12ae5e508feec7",
+    "url": "/event-axis/static/js/runtime~main.17ec6973.js"
+  },
+  {
+    "revision": "d0b0e272615b767a0c77",
+    "url": "/event-axis/static/js/2.956fa486.chunk.js"
+  },
+  {
+    "revision": "461ae141c5088bafdc9e",
+    "url": "/event-axis/static/js/3.ce65eebf.chunk.js"
+  },
+  {
+    "revision": "0bb1d5c962cc9f9cc698036d287e6841",
+    "url": "/event-axis/index.html"
+  }
+];
