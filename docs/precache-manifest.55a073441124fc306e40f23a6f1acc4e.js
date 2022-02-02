0e13e3275901d1465ecc4236ae81fce41679f7cf
Naom-Roads <nrrodrig@gmail.com>
2022-02-02 13:00:51 +0100

    Changed location to all in getData

---
+++ docs/precache-manifest.55a073441124fc306e40f23a6f1acc4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "714ac93bac5ccf08dbe2",
+    "url": "/event-axis/static/css/main.6a19cdd9.chunk.css"
+  },
+  {
+    "revision": "714ac93bac5ccf08dbe2",
+    "url": "/event-axis/static/js/main.b879e1f1.chunk.js"
+  },
+  {
+    "revision": "5b7890e1f29be0279dcb",
+    "url": "/event-axis/static/js/runtime~main.c19cb92e.js"
+  },
+  {
+    "revision": "4e9a3497a482f85052de",
+    "url": "/event-axis/static/js/2.a4863383.chunk.js"
+  },
+  {
+    "revision": "825f7bfd801c07bc4058",
+    "url": "/event-axis/static/js/3.25121d8a.chunk.js"
+  },
+  {
+    "revision": "e66784642d6c8eec644dc9a271dbfe49",
+    "url": "/event-axis/index.html"
+  }
+];
