66fc7452fa4c73614f09e95b0ae12fd669a4e072
Naom-Roads <nrrodrig@gmail.com>
2022-02-02 12:14:01 +0100

    Added responsive container and rebuilt docs

---
+++ docs/precache-manifest.07207af4fc6e3f55305a2c37d19374a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9ac532f50ed1dffd80d",
+    "url": "/event-axis/static/css/main.6a19cdd9.chunk.css"
+  },
+  {
+    "revision": "b9ac532f50ed1dffd80d",
+    "url": "/event-axis/static/js/main.e59302a4.chunk.js"
+  },
+  {
+    "revision": "5b7890e1f29be0279dcb",
+    "url": "/event-axis/static/js/runtime~main.c19cb92e.js"
+  },
+  {
+    "revision": "4e9a3497a482f85052de",
+    "url": "/event-axis/static/js/2.a4863383.chunk.js"
+  },
+  {
+    "revision": "825f7bfd801c07bc4058",
+    "url": "/event-axis/static/js/3.25121d8a.chunk.js"
+  },
+  {
+    "revision": "3497f4650a0a1861ae624f13e2d5a5d8",
+    "url": "/event-axis/index.html"
+  }
+];
